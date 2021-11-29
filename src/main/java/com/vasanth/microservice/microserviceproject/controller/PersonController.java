c6d8d0377f355f18a794da8600ccd583664157b7
vasanth <vasanthshetty775@gmail.com>
2021-11-30 01:26:12 +0530

    Created Customized exception handling and added proper Http status

--- src/main/java/com/vasanth/microservice/microserviceproject/controller/PersonController.java
+++ src/main/java/com/vasanth/microservice/microserviceproject/controller/PersonController.java
@@ -3,6 +3,9 @@ package com.vasanth.microservice.microserviceproject.controller;
 import com.vasanth.microservice.microserviceproject.entity.Person;
 import com.vasanth.microservice.microserviceproject.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,31 +20,33 @@ public class PersonController {
 
     //to list all person data
     @GetMapping("/person/list")
-    public List<Person> allPersonList(){
-        return personService.listOfPerson();
+    public ResponseEntity<List<Person>> allPersonList(){
+        return new ResponseEntity<List<Person>>(personService.listOfPerson(),HttpStatus.OK);
     }
 
     // to display a required person data
     @GetMapping("/person/{personId}")
-    public Person retrievePerson(@PathVariable int personId){    //@RequestParam
-        return personService.personDataById(personId);
+    public ResponseEntity<Person> retrievePerson(@PathVariable int personId){    //@RequestParam
+        //return personService.personDataById(personId);
+        Person person=personService.personDataById(personId);
+        return new ResponseEntity<Person>(person,HttpStatus.OK);
     }
 
     //to create a person
     @PostMapping("/person")
-    public Person createPerson(@RequestBody Person person){
-        return personService.addPerson(person);
+    public ResponseEntity<Person> createPerson(@RequestBody Person person){
+        return new ResponseEntity<Person>(personService.addPerson(person),HttpStatus.CREATED);
     }
 
-    @PostMapping("/person/{personId}")
-    public Person updatePerson(@PathVariable int personId,@RequestBody Person person){
+    @PutMapping("/person/{personId}")
+    public ResponseEntity<Person> updatePerson(@PathVariable int personId,@RequestBody Person person){
         Person existingPerson=personService.personDataById(personId);
         existingPerson.setId(personId);
         existingPerson.setFirstName(person.getFirstName());
         existingPerson.setLastName(person.getLastName());
         existingPerson.setCity(person.getCity());
 
-        return personService.addPerson(existingPerson);
+        return new ResponseEntity<Person>(personService.addPerson(existingPerson),HttpStatus.OK);
     }
 
     @DeleteMapping("/person/{personId}")
